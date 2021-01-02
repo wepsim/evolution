8813a84a7b9cb02b6069159b53aa1de322ef0dca
Alejandro Calderon Mateos <acaldero.uc3m@gmail.com>
2021-01-02 23:37:05 +0100

    Timeline format

--- index-timeline-h.js
+++ index-timeline-h.js
@@ -4,115 +4,115 @@ var timeline_data1 = [
     {
         time: '2015-09-15',
         body: [{
+            tag: 'img',
+            attr: {
+                src:      '1.0.10.jpg',
+                cssclass: 'img-responsive img-fluid py-0 my-0 zoom',
+                alt:      'screenshot of the wepsim 1.0.10'
+            }
+        },{
             tag: 'h4',
-            attr: { cssclass: 'container d-flex w-100 bg-light mb-2' },
+            attr: { cssclass: 'container d-flex w-100 bg-light py-2' },
             content: '<a href="1.0.10/" class="mx-auto">WepSIM 1.0.10</a>'
         },{
             tag: 'h5',
-            attr: { cssclass: 'container d-flex w-100 mx-0 bg-warning' },
+            attr: { cssclass: 'container d-flex w-100 mx-0 mb-2 bg-warning' },
             content: 'The initial release',
-        },{
-            tag: 'img',
-            attr: {
-		src:      '1.0.10.jpg',
-                cssclass: 'img-responsive img-fluid py-3 zoom',
-		alt:      'screenshot of the wepsim 1.0.10'
-            }
         }]
     },
     {
         time: '2016-12-11',
         body: [{
+            tag: 'img',
+            attr: {
+                src:      '1.7.2.jpg',
+                cssclass: 'img-responsive img-fluid py-0 my-0 zoom',
+                alt:      'screenshot of the wepsim 1.7.2'
+            }
+        },{
             tag: 'h4',
-            attr: { cssclass: 'container d-flex w-100 bg-light mb-2' },
+            attr: { cssclass: 'container d-flex w-100 bg-light py-2' },
             content: '<a href="1.7.2/" class="mx-auto">WepSIM 1.7.2</a>'
         },{
             tag: 'h5',
             attr: { cssclass: 'container d-flex w-100 mx-0 bg-warning' },
             content: 'Improved UI and faster execution speed',
-        },{
-            tag: 'img',
-            attr: {
-		src:      '1.7.2.jpg',
-                cssclass: 'img-responsive img-fluid py-3 zoom',
-		alt:      'screenshot of the wepsim 1.7.2'
-            }
         }]
     },
     {
         time: '2017-12-10',
         body: [{
+            tag: 'img',
+            attr: {
+                src:      '1.8.2.jpg',
+                cssclass: 'img-responsive img-fluid py-0 my-0 zoom',
+                alt:      'screenshot of the wepsim 1.8.2'
+            }
+        },{
             tag: 'h4',
-            attr: { cssclass: 'container d-flex w-100 bg-light mb-2' },
+            attr: { cssclass: 'container d-flex w-100 bg-light py-2' },
             content: '<a href="1.8.2/" class="mx-auto">WepSIM 1.8.2</a>'
         },{
             tag: 'h5',
             attr: { cssclass: 'container d-flex w-100 mx-0 bg-warning' },
             content: 'Initial support for command line, and the "state" management is added',
-        },{
-            tag: 'img',
-            attr: {
-		src:      '1.8.2.jpg',
-                cssclass: 'img-responsive img-fluid py-3 zoom',
-		alt:      'screenshot of the wepsim 1.8.2'
-            }
         }]
     },
     {
         time: '2018-11-25',
-        body: [{
+        body: [{ 
+            tag: 'img',
+            attr: {
+                src:      '1.9.3.jpg',
+                cssclass: 'img-responsive img-fluid py-0 my-0 zoom',
+                alt:      'screenshot of the wepsim 1.9.3'
+            }
+        },{
             tag: 'h4',
-            attr: { cssclass: 'container d-flex w-100 bg-light mb-2' },
+            attr: { cssclass: 'container d-flex w-100 bg-light py-2' },
             content: '<a href="1.9.3/" class="mx-auto">WepSIM 1.9.3</a>'
         },{
             tag: 'h5',
             attr: { cssclass: 'container d-flex w-100 mx-0 bg-warning' },
-            content: 'Support for several processors added, command line improvements, and updated Web UI'
-        },{
-            tag: 'img',
-            attr: {
-		src:      '1.9.3.jpg',
-                cssclass: 'img-responsive img-fluid py-3 zoom',
-		alt:      'screenshot of the wepsim 1.9.3'
-            }
+            content: 'Support for several CPUs added, command-line and Web UI improvements'
         }]
     },
     {
         time: '2019-11-20',
-        body: [{
+        body: [{ 
+            tag: 'img',
+            attr: {
+                src:      '2.0.10.jpg',
+                cssclass: 'img-responsive img-fluid py-0 my-0 zoom',
+                alt:      'screenshot of the wepsim 2.0.10'
+            }
+        },{
             tag: 'h4',
-            attr: { cssclass: 'container d-flex w-100 bg-light mb-2' },
+            attr: { cssclass: 'container d-flex w-100 bg-light py-2' },
             content: '<a href="2.0.10/" class="mx-auto">WepSIM 2.0.10</a>'
         },{
             tag: 'h5',
             attr: { cssclass: 'container d-flex w-100 mx-0 bg-warning' },
-            content: 'Introduction of checkpoint, pre-load, and recording support, plus many additional improvements'
-        },{
-            tag: 'img',
-            attr: {
-		src:      '2.0.10.jpg',
-                cssclass: 'img-responsive img-fluid py-3 zoom',
-		alt:      'screenshot of the wepsim 2.0.10'
-            }
+            content: 'Introduction of checkpoint, pre-load, and recording support'
         }]
     },
     {
         time: '2020-10-26',
         body: [{
+            tag: 'img',
+            attr: {
+                src:      '2.1.2.jpg',
+                cssclass: 'img-responsive img-fluid py-0 my-0 zoom',
+                alt:      'screenshot of the wepsim 2.1.2'
+            }
+        },{
             tag: 'h4',
-            attr: { cssclass: 'container d-flex w-100 bg-light mb-2' },
+            attr: { cssclass: 'container d-flex w-100 bg-light py-2' },
             content: '<a href="2.1.2/" class="mx-auto">WepSIM 2.1.2</a>'
         },{
             tag: 'h5',
             attr: { cssclass: 'container d-flex w-100 mx-0 bg-warning' },
-            content: 'Example sets, revised UI based on WebComponents, plus many additional improvements<br>'
-        },{
-            tag: 'img',
-            attr: {
-		src:      '2.1.2.jpg',
-                cssclass: 'img-responsive img-fluid py-3 zoom',
-		alt:      'screenshot of the wepsim 2.1.2'
-            }
+            content: 'Example sets, revised UI based on WebComponents, among other improvements'
         }]
     }
 ];
@@ -122,12 +122,11 @@ $(document).ready(function () {
 
     var timeline_cfg1 = {
 		           // effect: "tada",
-		           language: "en-us",
-                           effect: 'zoomInUp',
+                           // effect: 'zoomInUp',
                            // showGroup: true,
-		           // showMenu: false,
-		           // sortDesc: true,
-                           // showMenu: true
+                           // showMenu: true,
+		           sortDesc: false,
+		           language: "en-us"
 		        } ;
 
     $("#ws_evolution").albeTimeline(timeline_data1, timeline_cfg1) ;
