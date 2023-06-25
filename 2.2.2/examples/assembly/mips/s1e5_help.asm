51c819cd47e67dfee7c4415331e34225e4662790
Alejandro Calderon Mateos <acaldero.uc3m@gmail.com>
2023-06-25 14:49:33 +0200

    version 2.2.2 added

--- 2.2.2/examples/assembly/mips/s1e5_help.asm
+++ 2.2.2/examples/assembly/mips/s1e5_help.asm
@@ -13,21 +13,23 @@ main:
 
          # notify: skip1st:true showas:offcanvas glow:btn_run_stop_exebar1 scroll2current:true showdetails:register_file
          # notify: TIP: if-then
-         # notify: <li> In high-level:
-         # notify: <pre  class="font-monospace m-0 overflow-visible lh-1">
+         # notify: High-level:
+         # notify: <pre  class="font-monospace lh-1 m-0 overflow-visible">
          # notify:   int a=1;
          # notify:   int b=2;
          # notify: 
          # notify:   if (a < b):
-         # notify:       a = b;            </pre>
-         # notify: <li> In low-level:
-         # notify: <pre  class="font-monospace m-0 overflow-visible lh-1">
+         # notify:       a = b;
+         # notify: </pre>
+         # notify: Low-level:
+         # notify: <pre  class="font-monospace lh-1 m-0 overflow-visible">
          # notify:        li  $t1 1
          # notify:        li  $t2 2
          # notify:  if_1: blt $t1 $t2 t_1
          # notify:        b   e_1
          # notify:   t_1: move $t1 $t2
-         # notify:   e_1:                   </pre>
+         # notify:   e_1: 
+         # notify: </pre>
 
          # int a=1;
          # int b=2;
@@ -52,19 +54,21 @@ main:
          # notify: skip1st:true showas:offcanvas glow:btn_run_stop_exebar1 scroll2current:true showdetails:register_file
          # notify: TIP: if-then
          # notify: High-level:
-         # notify: <pre  class="font-monospace m-0 overflow-visible lh-1">
+         # notify: <pre  class="font-monospace lh-1 m-0 overflow-visible">
 	 # notify: int a=1;
 	 # notify: int b=2;
 	 # notify:
          # notify: if (a < b):
-	 # notify:     a = b;        </pre>
+	 # notify:     a = b;
+         # notify: </pre>
          # notify: Low-level:
-         # notify: <pre  class="font-monospace m-0 overflow-visible lh-1">
+         # notify: <pre  class="font-monospace lh-1 m-0 overflow-visible">
 	 # notify:       li t1 1
 	 # notify:       li t2 2
 	 # notify: if_2: bge t1 t2 e_2
 	 # notify: t_2: mv t1 t2
-	 # notify: e_2:              </pre>
+	 # notify: e_2:
+         # notify: </pre>
 
          # int a=1;
          # int b=2;
@@ -86,17 +90,18 @@ main:
 
          # notify: skip1st:true showas:offcanvas glow:btn_run_stop_exebar1 scroll2current:true showdetails:register_file
          # notify: TIP: if-then
-         # notify: <li> In high-level:
-         # notify: <pre  class="font-monospace m-0 overflow-visible lh-1">
+         # notify: High-level:
+         # notify: <pre  class="font-monospace lh-1 m-0 overflow-visible">
          # notify: int a=1;
          # notify: int b=2;
          # notify: 
          # notify: if (a < b):
          # notify:     &lt;assembly for action 1&gt;
          # notify: else:
-         # notify:     &lt;assembly for action 2&gt;   </pre>
-         # notify: <li> In low-level:
-         # notify: <pre  class="font-monospace my-4 overflow-visible lh-base">
+         # notify:     &lt;assembly for action 2&gt;
+         # notify: </pre>
+         # notify: Low-level:
+         # notify: <pre  class="font-monospace lh-1 m-0 overflow-visible">
          # notify:       li  t1 1
          # notify:       li  t2 2
          # notify: if_3: bge t1 t2 l_3
@@ -104,7 +109,8 @@ main:
          # notify:       beq x0 x0 end_3
          # notify:  l_3: &lt;assembly for action 2&gt;
          # notify:       beq x0 x0 end_3
-         # notify:  end_3:                    </pre>
+         # notify:  end_3:
+         # notify: </pre>
 
 
          # int a=1;
